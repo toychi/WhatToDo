48be11adb12269245f94b987661b017ee4460b63
Chitipat Thabsuwan <chitipat.toy@gmail.com>
2018-04-27 15:51:24 +0700

    Desk fragment adapter with mockup

--- app/src/main/java/com/example/toychi/whattodo/DeskFragment.java
+++ app/src/main/java/com/example/toychi/whattodo/DeskFragment.java
@@ -9,7 +9,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar;
@@ -20,10 +22,13 @@ import java.util.Date;
 
 public class DeskFragment extends Fragment {
 
+    public ProgressListAdapter adapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        adapter = new ProgressListAdapter(getActivity());
 
     }
 
@@ -34,39 +39,23 @@ public class DeskFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_desk, container, false);
 
         Date c = Calendar.getInstance().getTime();
-        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+        SimpleDateFormat df = new SimpleDateFormat("dd MMMM yyyy");
         String formattedDate = df.format(c);
         TextView textdate = view.findViewById(R.id.textdate);
         textdate.setText(formattedDate);
 
-        //Add Task to taskbox
-        LinearLayout taskBox = view.findViewById(R.id.taskbox);
-        int totalTask = 20;
-        for(int runningTask=0; runningTask<totalTask; runningTask++){
-            LinearLayout task = new LinearLayout(getActivity());
-            task.setOrientation(LinearLayout.VERTICAL);
+        // Progress Bar Adapter
+        ListView listView = view.findViewById(R.id.taskbox);
+        listView.setAdapter(adapter);
+        listView.setDivider(null);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent SubTaskView = new Intent(getActivity(), com.example.toychi.whattodo.SubTaskView.class);
+                startActivity(SubTaskView);
+            }
+        });
 
-            TextView t = new TextView(getActivity());
-            t.setText("Task 1");
-            t.setId(runningTask);
-            task.setPadding(0,15,0,15);
-            task.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent SubTaskView = new Intent(getActivity(), com.example.toychi.whattodo.SubTaskView.class);
-                    startActivity(SubTaskView);
-                }
-            });
-            task.addView(t);
-
-            //Progress bar
-            RoundCornerProgressBar progressBar = new RoundCornerProgressBar(getActivity(),null);
-            progressBar.setMax(5);
-            progressBar.setMinimumHeight(70);
-            progressBar.setProgress((1.0f*runningTask));
-            task.addView(progressBar);
-            taskBox.addView(task);
-        }
         return view;
     }
 
